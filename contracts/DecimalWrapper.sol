b6ac8549902047b17a7b1772531f634cbabbc47d
Mick de Graaf <mick@mickdegraaf.nl>
2021-03-22 18:14:06 +0100

    Update DecimalWrapper.sol

--- contracts/DecimalWrapper.sol
+++ contracts/DecimalWrapper.sol
@@ -13,6 +13,9 @@ contract DecimalWrapper is ERC20 {
   uint256 public conversion;
   IERC20 public underlying;
 
+  event Deposit(address indexed dst, uint wad);
+  event Withdrawal(address indexed src, uint wad);
+
   constructor(string memory _name, string memory _symbol, address _underlying, uint256 _conversion) ERC20(_name, _symbol) {
     underlying = IERC20(_underlying);
     conversion = _conversion;
@@ -21,10 +24,12 @@ contract DecimalWrapper is ERC20 {
   function deposit(uint256 _amount) external {
     underlying.safeTransferFrom(msg.sender, address(this), _amount);
     _mint(msg.sender, _amount.mul(conversion));
+    emit Deposit(msg.sender, _amount);
   }
 
   function withdraw(uint256 _amount) external {
     _burn(msg.sender, _amount);
     underlying.safeTransfer(msg.sender, _amount.div(conversion));
+    emit Withdrawal(msg.sender, _amount);
   }
 }
